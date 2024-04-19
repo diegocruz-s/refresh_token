4a99296fb07a3f01ba0cc603629f3e962bc74f53
Diego Cruz <diegoszcruz1001@gmail.com>
2024-04-19 14:57:41 -0300

    feat: add RefreshToken entity

---
+++ src/entities/RefreshToken.ts
@@ -0,0 +1,46 @@
+import { randomUUID } from "crypto";
+
+export interface IDatasRefreshToken {
+  expiresIn: Date;
+  userId: string
+};
+
+export interface IReturnValidation {
+  errors: string[] | null;
+  valid: boolean;
+};
+
+export class RefreshToken {
+  private readonly _id: string;
+  readonly expiresIn: Date;
+  readonly userId: string;
+
+  constructor({ expiresIn, userId }: IDatasRefreshToken) {
+    this._id = randomUUID();
+    this.expiresIn = expiresIn;
+    this.userId = userId;
+  };
+
+  get id (): string {
+    return this._id;
+  };
+
+  isValid(): IReturnValidation {
+    const errors = [];
+    if (!this.userId) {
+      errors.push('Property userId is missing!');
+    };
+
+    const isValidExpiresIn = this.expiresIn.getTime() > Date.now(); 
+
+    if (!isValidExpiresIn) {
+      errors.push('Property expiresIn must be greater than now!');
+    };
+
+    return {
+      errors: errors.length > 0 ? errors : null,
+      valid: errors.length > 0 ? false : true,
+    };
+  };
+
+};
